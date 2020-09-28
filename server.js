8ed1f2bc2cbd7c43c321b80f7a7dd38ad2d2f584
amacken <alex.mackenzie2@gmail.com>
2020-09-28 11:54:05 -0400

    Updated server.js with middleware, controllers, and listener

--- server.js
+++ server.js
@@ -1,7 +1,29 @@
+require("dotenv").config();
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT||3001;
+const mongoose = require("mongoose");
+const cors = require("cors");
 
+const MONGODB_URI = process.env.MONGODB_URI;
+const db = mongoose.connection;
+
+mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+db.on("open", () => {
+    console.log("Mongo is Connected");
+});
+
+// Middleware
+app.use(cors());
 app.use(express.json());
 
+// Controller
+app.use("/api/cities", require('./controllers/cityController'));
 
-app.listen(process.env.PORT||3000)
+// Listener
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+});
