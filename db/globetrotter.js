9df6fb19616bedb677a73a2c4c4924ca1d0a88ad
amacken <alex.mackenzie2@gmail.com>
2020-09-29 16:30:53 -0400

    globetrotter.js updated

--- db/globetrotter.js
+++ db/globetrotter.js
@@ -1,5 +1,31 @@
-const express = require('express');
-const router = express.Router();
+// const express = require('express');
+// const router = express.Router();
 
 
-module.exports = router;
+// module.exports = router;
+
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+const MONGODB_URI = process.env.MONGODB_URI;
+const db = mongoose.connection;
+
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+db.on("open", () => {
+  console.log("Mongo is Connected");
+});
+
+const data = require("./data");
+const City = require("../models/city");
+
+City.insertMany(data, (error, seededEntries) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(seededEntries);
+  }
+  process.exit();
+});
